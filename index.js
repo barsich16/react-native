0eb0f9a3e55e987bda72304f1c065daefe6b4671
barsich16 <bohdanborisenko8@gmail.com>
2023-09-26 20:17:43 +0300

    task 6

--- index.js
+++ index.js
@@ -1,11 +1,127 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation";
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 import {GradientScreen} from "./src/screens/GradientScreen";
 import {MaskedScreen} from "./src/screens/MaskedScreen";
 
-AppRegistry.registerComponent(appName, () => App);
+const HomeScreen = (props) => {
+    return (
+        <View style={styles.root}>
+            <Text>Hello React Native Navigation 👋</Text>
+            <Button
+                title='Push Settings Screens'
+                color='#710ce3'
+                onPress={() => Navigation.push(props.componentId, {
+                    component: {
+                        name: 'Settings',
+                        options: {
+                            topBar: {
+                                title: {
+                                    text: 'Settings'
+                                }
+                            }
+                        }
+                    }
+                })}/>
+        </View>
+    );
+};
+HomeScreen.options = {
+    topBar: {
+        title: {
+            text: 'Home',
+            color: 'white'
+        },
+        background: {
+            color: '#4d089a'
+        }
+    },
+    bottomTab: {
+        text: 'Home'
+    }
+}
+
+const SettingsScreen = () => {
+    return (
+        <View style={styles.root}>
+            <Text>Settings Screen</Text>
+        </View>
+    );
+}
+SettingsScreen.options = {
+    topBar: {
+        title: {
+            text: 'Settings'
+        }
+    },
+    bottomTab: {
+        text: 'Settings'
+    }
+}
+
+Navigation.registerComponent('Home', () => HomeScreen);
+Navigation.registerComponent('Settings', () => SettingsScreen);
+
+Navigation.setDefaultOptions({
+    statusBar: {
+        backgroundColor: '#4d089a'
+    },
+    topBar: {
+        title: {
+            color: 'white'
+        },
+        backButton: {
+            color: 'white'
+        },
+        background: {
+            color: '#4d089a'
+        }
+    },
+    bottomTab: {
+        fontSize: 14,
+        selectedFontSize: 14
+    }
+});
+
+Navigation.events().registerAppLaunchedListener(async () => {
+    Navigation.setRoot({
+        root: {
+            bottomTabs: {
+                children: [
+                    {
+                        stack: {
+                            children: [
+                                {
+                                    component: {
+                                        name: 'Home'
+                                    }
+                                },
+                            ]
+                        }
+                    },
+                    {
+                        stack: {
+                            children: [
+                                {
+                                    component: {
+                                        name: 'Settings'
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    });
+});
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'whitesmoke'
+    }
+});
